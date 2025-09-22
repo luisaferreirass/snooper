81512659ef37c2c9bd476b5f65ee018cf3d959dd
Luisa <luisaferreirass08@gmail.com>
2025-09-22 15:10:09 -0300

    feat: correcting github commits stats and implementing git lab commit stats class

--- src/main/java/br/com/jadson/snooper/github/data/commit/CommitStats.java
+++ src/main/java/br/com/jadson/snooper/github/data/commit/CommitStats.java
@@ -1,4 +1,10 @@
 package br.com.jadson.snooper.github.data.commit;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CommitStats {
+    public int additions;
+    public int deletions;
+    public int total;
 }
