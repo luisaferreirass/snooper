1bf12f4f571aa01cd1b8145faf95a079a43a7b26
Luisa <luisaferreirass08@gmail.com>
2026-08-10 20:48:04 -0300

    feat: adding git lab file stats data class

---
+++ src/main/java/br/com/jadson/snooper/gitlab/data/stats/GitLabCommitStats.java
@@ -0,0 +1,4 @@
+package br.com.jadson.snooper.gitlab.data.stats;
+
+public class GitLabCommitStats {
+}
---
+++ src/main/java/br/com/jadson/snooper/gitlab/data/stats/GitLabFileStats.java
@@ -0,0 +1,14 @@
+package br.com.jadson.snooper.gitlab.data.stats;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Churn (number of commits) information of a file in a GitLab repository.
+ *
+ * Luisa Ferreira - luisaferreirass08@gmail.com
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GitLabFileStats {
+    public String path;
+    public int churn;
+}
